17f5aeb57c9de5b207acc6057dedce8a226cd89d
Egor <e-okishev@mail.ru>
2023-07-01 23:44:06 +0300

    add search city

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,12 +1,12 @@
 import { fetchForecast, fetchWeather } from "./APIservice.js";
 import { renderWidgetForecast, renderWidgetOther, renderWidgetToday, showError } from "./render.js";
 
+export const startWidget = async (city, widget) => {
 
-export const startWidget = async () => {
-
-  const city = 'Киров';
-  const widget = document.createElement('div');
-  widget.classList.add('widget')
+  if(!widget) {
+    widget = document.createElement('div');
+    widget.classList.add('widget')
+  }
 
   const dataWeather = await fetchWeather(city)
 
--- scripts/widget.js
+++ scripts/widget.js
@@ -1,8 +1,12 @@
+import { cityServiceSearch } from "./modules/cityServiceSearch.js";
 import { startWidget } from "./modules/widgetService.js";
 
 const initWdget = async (app) => {
-  const widget = await startWidget()
+  const city = 'Кирово-Чепецк';
+  const widget = await startWidget(city)
   app.append(widget)
+
+  cityServiceSearch(widget);
 }
 
 initWdget(document.querySelector('#app'))
